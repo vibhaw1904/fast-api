183e44ab2c03c0d8a365fdddc793a862149b7684
vibhaw raj <vibhawraj494@gmail.com>
2025-07-03 10:05:07 +0530

    [post]:req

--- main.py
+++ main.py
@@ -25,3 +25,8 @@ def read_item(item_id: int, q: Union[str, None] = None):
 def update_item(item_id:int,item:Item):
     return {"item_name": item.name, "item_id": item_id}
 
+
+@app.post("/items/")
+def create_item(item:Item):
+    return {"item_name":item.name,"item_price":item.price,"item_is_offer":item.is_offer}
+
